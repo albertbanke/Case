36c30bcffdd3373e99aaba5b4241b0864b71631e
alleballer <albert.banke@gmail.com>
2023-02-18 11:30:32 +0100

    Indsat konklusion

--- analyse/app.py
+++ analyse/app.py
@@ -458,3 +458,27 @@ st.markdown("""
 _________
 """)
 
+
+st.markdown("""
+# Konklusion
+
+Denne notebook har fokuseret på rapporten fra Arbejdstilsynet om danskernes arbejdsmiljø i 2018. Ved hjælp af data wrangling blev datasættene gjort klar til analyse, og EDA viste nogle interessante resultater. 
+
+1. Arbejdsmiljøet scorer bedst i brancher med større selvstændighed og ansvar, typisk i det private.
+2. Arbejdsmiljøet scorer bedre for grupper med mænd og yngre personer.
+
+Time-series analyse og clustering modellering gav yderligere indsigt i dataet:
+
+3. Tiden til arbejdsopgaver for branchen undervisning er blevet mindre fra 2012 til 2018.
+4. K-means clustering og elbow-metoden viste, at det optimale antal klynger er 4 for køn og alder dataet. Machine learning skal anvendes med stor forsigtighed ved sensitive variabler som disse.
+
+Dataet er historisk og reflektering af udviklingen i arbejdsmiljøet på baggrund af findings er på sin plads. For eksempel kan strejkerne i 2021 blandt sygeplejersker relateres til den lavere vurdering af deres arbejdsmiljø, som blev vist i resultat 1. Kigger man på time-series analysen er en implikation de samfundsændringer som kan påvirke elementer som tid til arbejdsopgaver. Lovgivninger og reformer (såsom folkeskole- og gymnasiereformerne) skal holdes op sammen med dataudviklingen for at hjælpe med at forklare, hvorfor ændringerne ser ud som de gør. Til sidst skal clustering og machine learning anvendes med forsigtighed for sensitive variabler som køn og alder, da det kan føre til indsat bias i modeller, der påvirker menneskers liv.
+
+""")
+
+
+st.markdown("""
+_________
+""")
+
+
